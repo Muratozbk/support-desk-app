288a23e3f59b27e38f7655031c22bb1b0851cfd5
muratozbk <slipstream11@yandex.com>
2023-06-07 14:45:42 +0300

    server errorhandler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000;
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     // app.get('/', (req, res) => {
@@ -12,6 +16,8 @@ app.get('/', (req, res) => {
 
 //import Routes useRoutes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
